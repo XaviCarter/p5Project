6e9cbd5b26e74e316f03cf4e0fb0479071d4c8d7
Xavi Carter <116882813+XaviCarter@users.noreply.github.com>
2022-11-29 10:56:50 +0000

    Add files via upload

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,32 @@
 function setup() {
-    var canvas = createCanvas(800, 800);
+    var canvas = createCanvas(1600, 1200);
     canvas.parent("p5container");
+    
 }
 
 function draw() {
-    background(220);
+    background(0);
 
-    fill(255, 255, 255);
-    ellipse(400, 400, 200);
+    //outline
+    fill('#F9F108');
+    rect(0, 0, 50, 1200);
+
+    fill('#F9F108');
+    rect(1550, 0, 50, 1200);
+
+    fill('#F9F108');
+    rect(0, 0, 1600, 50);
+
+    fill('#F9F108');
+    rect(0, 1150, 1600, 50);
+    
+    //radiation symbol
+    fill('#F9F108');
+    ellipse(800, 600, 200);
+
+    fill(0, 0, 0);
+    ellipse(800,600, 30);
+    
+    fill('FFFFFF');
+    rect(200, 200, 100, 100, 55, 55, 0, 55)
 }
